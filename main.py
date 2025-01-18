aef6f800b3118555efe151597a4e2007d4491474
Szymon <szymon.marciniak0@icloud.com>
2025-01-18 12:02:12 +0100

    Changed normal queue to priority queue

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 Projekt na laboratorium przedmiotu Algorytmy i Struktury Danych.
 """
 import logging
+from datetime import datetime, timedelta
+from enum import Enum
+
+class Gender(Enum):
+    """
+    Enum przechowujący płeć
+    """
+    K = 'Kobieta'
+    M = 'Mężczyzna'
 
 class Patient:
     """
@@ -18,11 +27,13 @@ class Patient:
         :param gender: To string mówiący o tym, jakiej płci jest pacjent.
         :param time_of_visit: To godzina o, której miał pacjent umówioną wizytę.
         """
+        if not isinstance(gender,Gender):
+            raise Exception("Płeć to musi być 'Kobieta' lub  'Mężczyzna'")
         self.name = name
         self.surname = surname
         self.age = age
         self.pesel = pesel
-        self.gender =gender
+        self.gender = gender
         self.time_of_visit = time_of_visit #Pytanie co do godziny czy importujemy time, czy zwykły string
 
         self. next_in_line = None
@@ -55,57 +66,102 @@ class PatientQueue:
    Klasa, która przechowuje informacje o pacjentach czekających do lekarza.
    """
    def __init__(self):
-       self.head = None
+       self.heap = []
 
    def add_patient(self, patient):
        """
-       Funkcja dodająca pacjenta na koniec kolejki.
-       :param patient: Przechowuje pacjenta, który dochodzi do kolejki
-       :return: None
+       Dodaje pacjenta do kopca i przywraca porządek kopca minimalnego.
        """
-       if patient is not None:
-           if self.head is None:
-               self.head = patient
-           else:
-               current_patient = self.head
-               while current_patient.next_in_line is not None:
-                   current_patient = current_patient.next_in_line
-               current_patient.next_in_line = patient
+       self.heap.append(patient)
+       self._build_heap()
+
+   def get_next_patient(self):
+       """
+       Funkcja, która usuwa pierwszego pacjenta z kolejki i przywraca właściwość kopca.
+       """
+       if self.is_empty():
+           print("Wszyscy pacjenci zostali zbadani")
+           logging.info("Wszyscy pacjenci zostali zbadani")
+           return
+       self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+       next_patient = self.heap.pop()
+       self._heapify_min(len(self.heap), 0)
+       return next_patient
+
+   def is_empty(self):
+       """
+       Sprawdza, czy kolejka jest pusta.
+       :return: Zwraca True, kiedy kolejka jest pusta, a False, jeżeli jest jakiś element.
+       """
+       return len(self.heap) == 0
+
+   def _build_heap(self):
+       """
+       Buduje kopiec minimalny z listy.
+       """
+       n = len(self.heap)
+       for i in range(n // 2 - 1, -1, -1):
+           self._heapify_min(n, i)
+
+   def _heapify_min(self, n, i):
+       """
+       Funkcja odpowiedzialna za tworzenie kopca minimalnego.
+       :param n: To długość danych wejściowych, z jakiej będzie zrobiony kopiec.
+       :param i: To miejsce, od którego przywracamy porządek kopca minimalnego.
+       """
+       smallest = i #indeks najmniejszego elementu, na początku 0
+       left_child = 2 * i + 1 #lewy potomek, badanego węzła
+       right_child = 2 * i + 2 #prawy potomek, badanego węzła
+
+       if left_child < n and self.heap[left_child].time_of_visit < self.heap[smallest].time_of_visit:
+           smallest = left_child
+       if right_child < n and self.heap[right_child].time_of_visit < self.heap[smallest].time_of_visit:
+           smallest = right_child
+       if smallest != i:
+           self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+           self._heapify_min(n, smallest)
 
    def display_queue(self):
        """
        Funkcja wyświetlająca dane pacjentów z kolejki
        :return: None
        """
-       if self.head is None:
-           logging.info("Kolejka jest pusta")
+       if len(self.heap) == 0:
+           print("Kolejka jest pusta")
+           logging.log(1,"Kolejka jest pusta")
        else:
-           current = self.head
-           while current is not None:
-               logging.info(
-                   f"Imię: {current.name}, Nazwisko: {current.surname}, Wiek: {current.age}, "
-                   f"PESEL: {current.pesel}, Płeć: {current.gender}, Godzina wizyty: {current.time_of_visit}"
-               )
-               current = current.next_in_line
+          for patient in self.heap:
+              patient.display()
+              logging.info(patient.display())
 
-   def insert_patient(self, patient, place):
+   def insert_patient(self, patient, position):
        """
-       Funkcja pozwalająca na dodanie pacjenta na dowolnym miejscu w kolejce.
-       :param patient: Przechowuje pacjenta wkładanego do kolejki
-       :param place: Pozycja na, którą chcemy włożyć pacjenta
-       :return: None
-       """
-       if place <= 0 or self.head is None:
-           patient.next_in_line = self.head
-           self.head = patient
-       else:
-           current_patient = self.head
-           count = 1
-           while count < place and current_patient.next_in_line is not None:
-               current_patient = current_patient.next_in_line
-               count += 1
-           patient.next_in_line = current_patient.next_in_line
-           current_patient.next_in_line = patient
+        Wstawia pacjenta priorytetowego w określone miejsce w kolejce.
+        Jeśli pacjent nie ma określonej godziny wizyty, ustala ją na podstawie
+        czasu pomiędzy sąsiadującymi pacjentami.
+
+        :param patient: To pacjent, którego będziemy dodawać do kolejki.
+        :param position: Indeks, w którym pacjent powinien być wstawiony.
+        """
+
+       if position < 0 or position > len(self.heap):
+            print("Niewłaściwa pozycja")
+            logging.info("Niewłaściwa pozycja")
+            return
+       if not patient.time_of_visit:
+            if 0 < position < len(self.heap):
+                prev_time = self.heap[position - 1].time_of_visit
+                next_time = self.heap[position].time_of_visit
+                patient.time_of_visit = prev_time + (next_time - prev_time) / 2
+            elif position == 0:
+                patient.time_of_visit = self.heap[0].time_of_visit - timedelta(minutes=1)
+            else:
+                patient.time_of_visit = self.heap[-1].time_of_visit + timedelta(minutes=1)
+
+       # Wstawienie pacjenta na określoną pozycję
+       self.heap.insert(position, patient)
+       self._build_heap()
+
 
 
    def remove_patient(self, position = 0):
@@ -114,19 +170,11 @@ class PatientQueue:
        :param position: Pozycja, z której zostanie usunięty pacjent
        :return: None
        """
-       if self.head is None:
-           logging.info("Kolejka jest pusta")
-           return
-       if position == 0:
-           self.head = self.head.next_in_line
+       if len(self.heap) == 0:
+           print("Nie ma pacjenta, którego można usunąć")
+           logging.info("Nie ma pacjenta, którego można usunąć")
        else:
-           current_patient = self.head
-           count = 1
-           while count < position and current_patient.next_in_line is not None:
-               current_patient = current_patient.next_in_line
-               count += 1
-           if current_patient.next_in_line is not None:
-               current_patient.next_in_line = current_patient.next_in_line.next_in_line
+           self.heap.pop(position)
 
 def create_patient(): #To trzeba będzie uodpornić na idiotów! O ile chcemy tę funkcję
     """
